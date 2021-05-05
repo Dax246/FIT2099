94623953f85e4f3190f3326b8f60324da078ad04
Damien <damb0001@student.monash.edu>
2021-05-06 08:45:57 +1000

    Added Work Breakdown Agreement for Assignment 2

--- src/game/Dirt.java
+++ src/game/Dirt.java
@@ -1,6 +1,9 @@
 package game;
 
+import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Ground;
+import edu.monash.fit2099.engine.Location;
+import java.util.Random;
 
 /**
  * A class that represents bare dirt.
@@ -9,6 +12,50 @@ public class Dirt extends Ground {
 
 	public Dirt() {
 		super('.');
+	}
+
+	@Override
+	public void tick(Location location) {
+		//FIXME: Add check to see if on the edge of gameMap
+		int bush_counter = 0;
+		boolean tree_check = false;
+		int x_coord = location.x();
+		int y_coord = location.y();
+		GameMap gameMap = location.map();
+
+		if (gameMap.at(x_coord+1, y_coord).getGround() instanceof Bush){
+			bush_counter ++;
+		}
+		if (gameMap.at(x_coord+1, y_coord).getGround() instanceof Tree){
+			bush_counter ++;
+		}
+		if (gameMap.at(x_coord-1, y_coord).getGround() instanceof Bush){
+			bush_counter ++;
+		}
+		if (gameMap.at(x_coord-1, y_coord).getGround() instanceof Tree){
+			bush_counter ++;
+		}
+		if (gameMap.at(x_coord, y_coord+1).getGround() instanceof Bush){
+			bush_counter ++;
+		}
+		if (gameMap.at(x_coord, y_coord+1).getGround() instanceof Tree){
+			bush_counter ++;
+		}
+		if (gameMap.at(x_coord, y_coord-1).getGround() instanceof Bush){
+			bush_counter ++;
+		}
+		Random random = new Random();
+		int bushChance = random.nextInt(100);
+		if (bush_counter >= 2 && 0 <= bushChance && bushChance <= 10){
+			// change to bush
+		}
+		if (bush_counter < 2 && bushChance == 0){
+			// change to bush
+		}
+
+
+
+		location
 	}
 
 	//tick(Location):
