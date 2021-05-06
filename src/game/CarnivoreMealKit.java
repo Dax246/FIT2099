70f14a6d2f8002d8bad34452a8f54310f1d0018a
Allan C <allanchan960@gmail.com>
2021-05-07 03:41:29 +1000

    Corrected flora location within the game package.

--- src/game/CarnivoreMealKit.java
+++ src/game/CarnivoreMealKit.java
@@ -11,27 +11,4 @@ public class CarnivoreMealKit extends Item {
         super("carnivore meal kit", 'C', true);
     }
 
-    public abstract static class Flora extends Ground {
-        private List<Fruit> grownFruits = new ArrayList<>();
-
-
-
-        public Flora(char displayChar) {
-            super(displayChar);
-        }
-
-        public int numberOfFruit(){
-            return grownFruits.size();
-        };
-
-        public List<Fruit> getGrownFruits() {
-            return grownFruits;
-        }
-
-        public Fruit getFruit(){
-            Fruit fruit = getGrownFruits().get(0);
-            getGrownFruits().remove(0);
-            return fruit;
-        }
-    }
 }
---
+++ src/game/Flora.java
@@ -0,0 +1,29 @@
+package game;
+
+import edu.monash.fit2099.engine.Ground;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Flora extends Ground {
+    private List<Fruit> grownFruits = new ArrayList<>();
+
+
+    public Flora(char displayChar) {
+        super(displayChar);
+    }
+
+    public int numberOfFruit() {
+        return grownFruits.size();
+    }
+
+    public List<Fruit> getGrownFruits() {
+        return grownFruits;
+    }
+
+    public Fruit getFruit() {
+        Fruit fruit = getGrownFruits().get(0);
+        getGrownFruits().remove(0);
+        return fruit;
+    }
+}
