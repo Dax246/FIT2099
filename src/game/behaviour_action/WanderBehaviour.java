d0d3cdc7fbf440a03491698facee758fd596ff20
Allan C <allanchan960@gmail.com>
2021-05-08 18:52:45 +1000

    Added DoNothingAction to wander behaviour if dinos have no other moves.

--- src/game/behaviour_action/WanderBehaviour.java
+++ src/game/behaviour_action/WanderBehaviour.java
@@ -3,11 +3,7 @@ package game.behaviour_action;
 import java.util.ArrayList;
 import java.util.Random;
 
-import edu.monash.fit2099.engine.Action;
-import edu.monash.fit2099.engine.Actor;
-import edu.monash.fit2099.engine.Exit;
-import edu.monash.fit2099.engine.GameMap;
-import edu.monash.fit2099.engine.Location;
+import edu.monash.fit2099.engine.*;
 import game.Behaviour;
 
 public class WanderBehaviour implements Behaviour {
@@ -38,7 +34,7 @@ public class WanderBehaviour implements Behaviour {
 			return actions.get(random.nextInt(actions.size()));
 		}
 		else {
-			return null;
+			return new DoNothingAction();
 		}
 
 	}
